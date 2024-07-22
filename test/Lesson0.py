063c32c31cecaec449c377d8531010b13d2942fa
trungsliver <buiductrung.cva@gmail.com>
2024-07-22 23:09:24 +0700

    test

--- test/Lesson0.py
+++ test/Lesson0.py
@@ -28,3 +28,49 @@ class Shiba(Dog):
 
 sb1 = Shiba('hihi', 5)
 print(sb1.get_description())
+
+# Tính đa hình (Polymorphism)
+class Dog:
+    tail = "long"
+    def __init__(self, name, age):
+        self.name = name              
+        self.age = age              
+        self.__sound = "Woof"
+
+    def get_description(self):
+        return f"{self.name} is {self.age}-year-old"
+    def speak(self):
+        return self.__sound
+
+
+class Corgi(Dog):
+    tail = "short"
+    def __init__(self, name, age):
+        super().__init__(name, age) 
+    # override
+    def get_description(self):
+        return f"{self.name} is a {self.age}-year-old corgi"
+
+# Tính trừu tượng (Abstraction)
+class Dog:
+      def __init__(self, name, age):
+            self.name = name              
+            self.age = age              
+
+
+      def speak(self):
+            pass
+
+class Shiba(Dog):
+      def __init__(self, name, age):
+            super().__init__(name, age)
+     
+      def speak(self):
+            print("Woof")
+
+class Husky(Dog):
+      def __init__(self, name, age):
+            super().__init__(name, age)
+     
+      def speak(self):
+            print("Howl")
--- test/test34.py
+++ test/test34.py
@@ -1,4 +1,11 @@
 from Lesson0 import Dog
 
 dog1 = Dog('Lu', 3)
+print(dog1)
 print(dog1.get_description())
+
+# Các nguyên lý/tính chất trong lập trình hướng đối tượng
+    # 1. Tính đóng gói
+    # 2. Tính kế thừa
+    # 3. Tính đa hình
+    # Tính trừu tượng
