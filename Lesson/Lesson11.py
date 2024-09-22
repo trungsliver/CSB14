b8f14b84e8fc79daefdbb2f3c95bb34e8f1ab69a
trungsliver <buiductrung.cva@gmail.com>
2024-09-22 19:14:19 +0700

    Lesson 11

---
+++ Lesson/Lesson11.py
@@ -0,0 +1,13 @@
+# STACK - ngăn xếp
+# Nguyên tắc: LIFO - Last in, First out
+ 
+# Khởi tạo stack rỗng
+stack = []
+
+# Push - Thêm phần tử vào stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+# Pop - Lấy phần tử cuối cùng và loại bỏ
+print(stack.pop())  # Xuất 3
