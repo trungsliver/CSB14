2d11704394c0b1a4fec6a9774262c4d8b4ce5798
trungsliver <buiductrung.cva@gmail.com>
2024-10-06 19:33:43 +0700

    Lesson 13

---
+++ Lesson/Lesson13.py
@@ -0,0 +1,63 @@
+# Review kiến thức
+
+# Buổi 1: Python Cơ bản
+    # Variables & Data Types
+    # Operators (phép toán cơ bản + biểu thức logic)
+    # Câu điều kiện
+    # Vòng lặp (for - while)
+
+# Buổi 2: List/Aray, Function, Files
+    # List/Array: CRUD
+    # Function: Tạo hàm, truyền tham số, trả về giá trị
+    # Files: Đọc, ghi file
+
+# Buổi 3+4: OOP - Lập trình hướng đối tượng
+    # Class: Tạo class, tạo đối tượng
+    # 4 tính chất của OOP: Đóng gói, Kế thừa, Đa hình, Trừu tượng
+
+# Buổi 5: Thuật toán
+    # Các khái niệm thuật toán
+    # Các tính chất đặc trưng
+    # Phân tích tính hiệu quả 
+    # Đánh giá thuật toán bằng độ phức tạp
+
+# Buổi 6: Thuật toán tìm kiếm
+    # Thuật toán tìm kiếm tuyến tính (Linear Search)
+    # Thuật toán tìm kiếm nhị phân (Binary Search)
+
+# Buổi 7: Thuật toán sắp xếp
+    # Sắp xếp chèn (Insertion sort)
+    # Sắp xếp bong bóng (Bubble sort)
+
+# Buổi 8+9: Ôn tập và kiểm tra
+
+# Buổi 10: Tập hợp và ánh xạ
+    # Cấu trúc dữ liệu Set (tập hợp)
+    # Cấu trúc dữ liệu Map (ánh xạ)
+    # Dictionary và ứng dụng
+
+# Buổi 11: Ngăn xếp (stack)
+    # Cách hoạt động của stack
+    # Các thao tác cơ bản trên stack
+    # Ứng dụng của stack (giả lập trình duyệt)
+
+# Buổi 12: Hàng đợi (queue)
+    # Cách hoạt động của queue
+    # Các thao tác cơ bản trên queue
+    # Ứng dụng của stack (Ứng dụng âm nhạc)
+
+# Yêu cầu chuẩn bị cho bài kiểm tra cuối khóa CSB
+    # 1. Ôn tập và nắm vững kiến thức:
+            # Chọn kiến thức của 2 buổi học ở trên để chuẩn bị & thông báo cho GV
+            # Buổi 14: GV sẽ yêu cầu trình bày về kiến thức 1 trong 2 buổi mà bạn đã chọn
+            # Buổi 14: GV sẽ đưa ra câu hỏi vấn đáp và bài tập thực hành (không sd hỗ trợ)
+    # 2. Làm đề kiểm tra TẠI NHÀ:
+            # Đề sẽ được gửi trong buổi 13 (zalo)
+            # Deadline: 12h đêm ngày 12/10/2024
+
+# Lựa chọn kiểm tra:
+# 1. Chu Quang Huy:
+# 2. Nguyễn Gia Bình:
+# 3. Trần Thành Đạt:
+# 4. Lê Việt Anh:
+# 5. Bùi Khánh Ngọc Nghi:
